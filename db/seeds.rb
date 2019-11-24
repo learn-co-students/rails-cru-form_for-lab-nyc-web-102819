64f15fd592d03c4677b3dc95f9a5a281af1e1df8
lukewaring <lukewaring@gmail.com>
2019-11-24 11:27:21 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,20 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 'Emanuel', city: cities.first
+
+Song.destroy_all
+Song.create(name: "Song 1")
+Song.create(name: "Song 2")
+Song.create(name: "Song 3")
+
+Artist.destroy_all
+Artist.create(name: "Artist 1", bio: "Bio 1")
+Artist.create(name: "Artist 2", bio: "Bio 2")
+Artist.create(name: "Artist 3", bio: "Bio 3")
+
+Genre.destroy_all
+Genre.create(name: "Genre 1")
+Genre.create(name: "Genre 2")
+Genre.create(name: "Genre 3")
+
